72ce9ab9feb477e5292cb03b56dad45cbe09b201
ahnafiiz <ahnaftahmeed32@gmail.com>
2026-09-07 10:46:58 +0000

    new bare

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://bare.bareproxy.workers.dev/',
+    bare: 'bare-server-workers-tau.vercel.app',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
